d78749018c0bba74fa4d858a0928f03d9e3a1160
Eric LING <eriling@e2r5p18.42.fr>
2021-03-23 18:23:49 +0100

    simplify hit_sphere function

--- comput/hit_sphere.c
+++ comput/hit_sphere.c
@@ -6,7 +6,7 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 14:08:28 by eriling           #+#    #+#             */
-/*   Updated: 2021/03/23 17:59:23 by eriling          ###   ########.fr       */
+/*   Updated: 2021/03/23 18:21:58 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,47 +16,49 @@
 #include <math.h>
 
 
+int intersection_sphere(t_math *math, double *t)
+{
+	if (math->det > 0)
+	{
+		math->t1 = (-math->b + sqrt(math->det)) / (2 * math->a);
+		math->t2 = (-math->b - sqrt(math->det)) / (2 * math->a);
+		if (math->t1 < math->t2 && math->t1 < *t)
+		{
+			*t = math->t1;
+			return (1);
+		}
+		else if (math->t2 < math->t1 && math->t2 < *t)
+		{
+			*t = math->t2;
+			return (1);
+		}
+	}
+	else if (math->det == 0)
+	{
+		math->t1 = - 0.5 * math->b / math->a;
+		if (math->t1 < *t)
+		{
+			*t = math->t1;
+			return (1);
+		}
+	}
+	return(0);
+}
+
 int	hit_sphere(t_vect dir, t_obj *cam, t_obj *sp, double *t)
 {
 	t_vect	v_cam;
-	
-	double	a;
-	double	b;
-	double	c;
-	double	det;
-	double t1;
-	double t2;
+	t_math	math;
 
 	v_cam.x = cam->u.camera.x - sp->u.sphere.x;
 	v_cam.y = cam->u.camera.y - sp->u.sphere.y;
 	v_cam.z = cam->u.camera.z - sp->u.sphere.z;
-	a = 1;
-	b = 2.0 * dot(v_cam, dir);
-	c = dot(v_cam, v_cam) - (sp->u.sphere.diam / 2) * (sp->u.sphere.diam / 2);
-	det = b * b - 4 * a * c;
-	if (det > 0)
-	{
-		t1 = (-b + sqrt(det)) / (2 * a);
-		t2 = (-b - sqrt(det)) / (2 * a);
-		if (t1 < t2 && t1 < *t)
-		{
-			*t = t1;
-			return (1);
-		}
-		else if (t2 < t1 && t2 < *t)
-		{
-			*t = t2;
-			return (1);
-		}
-	}
-	else if (det == 0)
-	{
-		t1 = - 0.5 * b / a;
-		if (t1 < *t)
-		{
-			*t = t1;
-			return (1);
-		}
-	}
+	math.a = 1;
+	math.b = 2.0 * dot(v_cam, dir);
+	math.c = dot(v_cam, v_cam) - (sp->u.sphere.diam / 2) * (sp->u.sphere.diam / 2);
+	math.det = math.b * math.b - 4 * math.a * math.c;
+
+	if (intersection_sphere(&math, t) == 1)
+		return (1);
 	return (0);
 }
