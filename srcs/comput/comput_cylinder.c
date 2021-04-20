a82f7d6d0c7a287739594cf0c5b779f76953bbbe
Eric LING <eric_ling@hotmail.fr>
2021-04-20 11:47:24 +0200

    I M A GOOOOOOOOOOOOOD

--- srcs/comput/comput_cylinder.c
+++ srcs/comput/comput_cylinder.c
@@ -6,27 +6,23 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 15:44:52 by eriling           #+#    #+#             */
-/*   Updated: 2021/04/19 23:40:42 by eriling          ###   ########.fr       */
+/*   Updated: 2021/04/20 11:45:57 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "struct.h"
 #include "comput.h"
+#include "miniRT.h"
+#include "print_struct.h"
 
 t_vect	get_normal_cylinder(t_data *img)
 {
-	double	xp;
-	double	yp;
 	t_vect	big_x;
 	t_vect	big_y;
 	t_vect	p;
 
 	get_bigx_bigy(&big_x, &big_y, img->obj);
-	xp = img->t * dot(img->dir, big_x)
-		+ dot(vect_sous(img->origin, vect_obj(img->obj)), big_x);
-	yp = img->t * dot(img->dir, big_y)
-		+ dot(vect_sous(img->origin, vect_obj(img->obj)), big_y);
-	p = init_vect(xp, yp, img->zp);
-	return (vect_sous(p, vect_sum(vect_obj(img->obj),
-		scale(vect_orien_cylinder(img->obj), img->zp))));
+	p = vect_sum(img->origin, scale(img->dir, img->t));
+	return (normalize(vect_sous(p, vect_sum(vect_obj(img->obj),
+		scale(normalize(vect_orien_cylinder(img->obj)), img->zp)))));
 }
--- srcs/comput/hit_cylinder.c
+++ srcs/comput/hit_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 12:48:03 by eriling           #+#    #+#             */
-/*   Updated: 2021/04/19 23:36:22 by eriling          ###   ########.fr       */
+/*   Updated: 2021/04/20 11:41:20 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,19 @@ void get_bigx_bigy(t_vect *big_x, t_vect *big_y, t_obj *cy)
 	if (vect_orien_cy.x == 0 && (vect_orien_cy.y == 1 || vect_orien_cy.y == -1)
 		&& vect_orien_cy.z == 0)
 		*big_x = init_vect(1, 0, 0);
-	*big_x = cross_prod(*big_x, vect_orien_cy);
-	*big_y = cross_prod(vect_orien_cy, *big_x);
+	*big_x = normalize(cross_prod(*big_x, vect_orien_cy));
+	*big_y = normalize(cross_prod(vect_orien_cy, *big_x));
 }
 
 double	get_zp(double t, t_data *img, t_obj *cy)
 {
 	double	zp;
-	t_vect vect_orien_cy;
 	t_vect	big_z;
 
-	vect_orien_cy = normalize(vect_orien_cylinder(cy));
-	big_z = vect_orien_cylinder(cy);
+	big_z = normalize(vect_orien_cylinder(cy));
 	zp = t * dot(img->dir, big_z)
-		+ dot(vect_sous(img->origin, vect_orien_cylinder(cy)), big_z);
-	if (fabs(zp) <= cy->u.cylinder.high / 2)
+		+ dot(vect_sous(img->origin, vect_obj(cy)), big_z);
+	if (fabs(zp) <= cy->u.cylinder.high / 2.0)
 		return (zp);
 	return (cy->u.cylinder.high + 1.0);
 }
@@ -48,7 +46,7 @@ int	is_det_zero_inter_near(t_math *math, t_data *img, t_obj *cy)
 {
 	math->t1 = -0.5 * math->b / math->a;
 	if (math->t1 > 0 && math->t1 < img->t 
-		&& get_zp(math->t1, img, cy) <= cy->u.cylinder.high / 2)
+		&& get_zp(math->t1, img, cy) <= cy->u.cylinder.high / 2.0)
 	{
 		img->t = math->t1;
 		return (1);
@@ -60,16 +58,16 @@ int is_inter_in_xp_yp_zp(t_math *math, t_data *img, t_obj *cy)
 {
 	if (math->det > 0)
 	{
-		math->t1 = (-math->b + sqrt(math->det)) / (2 * math->a);
-		math->t2 = (-math->b - sqrt(math->det)) / (2 * math->a);
+		math->t1 = (-math->b + sqrt(math->det)) / (2.0 * math->a);
+		math->t2 = (-math->b - sqrt(math->det)) / (2.0 * math->a);
 			if ((math->t1 < 0 || math->t1 > img->t)
 				&& (math->t2 < 0 || math->t2 > img->t))
 				return (0);
 			if (math->t1 > 0 && math->t1 < img->t 
-				&& get_zp(math->t1, img, cy) <= cy->u.cylinder.high / 2)
+				&& get_zp(math->t1, img, cy) <= cy->u.cylinder.high / 2.0)
 				img->t = math->t1;
 			if (math->t2 > 0 && math->t2 < img->t 
-				&& get_zp(math->t2, img, cy) <= cy->u.cylinder.high / 2)
+				&& get_zp(math->t2, img, cy) <= cy->u.cylinder.high / 2.0)
 				img->t = math->t2;
 			return (1);
 	}
@@ -89,12 +87,12 @@ int	hit_cylinder(t_vect dir, t_vect origin, t_data *img, t_obj *cy)
 	img->origin = origin;
 	img->dir = dir;
 	math.a = pow(dot(dir, big_x), 2) + pow(dot(dir, big_y), 2);
-	math.b = 2 * (dot(dir, big_x) * dot(vect_sous(origin, vect_obj(cy)), big_x))
-		+ dot(dir, big_y) * dot(vect_sous(origin, vect_obj(cy)), big_y);
+	math.b = 2.0 * (dot(dir, big_x) * dot(vect_sous(origin, vect_obj(cy)), big_x)
+		+ dot(dir, big_y) * dot(vect_sous(origin, vect_obj(cy)), big_y));
 	math.c = pow(dot(vect_sous(origin, vect_obj(cy)), big_x), 2)
 		+ pow(dot(vect_sous(origin, vect_obj(cy)), big_y), 2)
 		- cy->u.cylinder.diam;
-	math.det = math.b * math.b - 4 * math.a * math.c;
+	math.det = math.b * math.b - 4.0 * math.a * math.c;
 	if (is_inter_in_xp_yp_zp(&math, img, cy) == 1)
 	{
 		img->zp = get_zp(img->t, img, cy);
